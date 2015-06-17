5cb6cfa6ccf69691da8738b5ae68eca5ae3fb16f
ravi.ved <ravi.ved@gmail.com>
2015-06-17 09:56:39 +0530

    Freemarker Template Example - m101j

---
+++ m101j/src/main/java/ravived/m101j/ch1/HelloWorldFreemarker.java
@@ -0,0 +1,42 @@
+package ravived.m101j.ch1;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by rved on 17/06/15.
+ */
+
+public class HelloWorldFreemarker {
+    public static void main(String[] args) {
+        //in start of classpath of HelloWorldFreemarker is whre u find html
+        //define template in resources directory-- maven and intellij know to include this in classpath
+        Configuration configuration = new Configuration();
+        configuration.setClassForTemplateLoading(HelloWorldFreemarker.class, "/");
+
+        try {
+            Template helloTemplate = configuration.getTemplate("hello.ftl");
+
+            StringWriter stringWriter = new StringWriter();
+            Map<String, Object> helloMap = new HashMap<String, Object>();
+            helloMap.put("name", "Freemarker !");
+
+            helloTemplate.process(helloMap, stringWriter);
+
+            System.out.println(stringWriter.toString());
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+        }
+
+    }
+
+
+}
---
+++ m101j/src/main/resources/hello.ftl
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Getting Started Freemarker</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+</head>
+<body>
+
+<h1>Hello ${name}</h1>
+</body>
+
+</html>
